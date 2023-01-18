eb9282711a3bbf89b7d9cd6bdc7d7b46b711421e
mannyrveloz23 <you@example.com>
2023-01-17 23:09:21 -0500

    m

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,18 @@
 // TODO: Include packages needed for this application
-const inquirer = require("inquirer")
-const fs = require('fs')
+const inquirer = require("inquirer");
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
+const licenseList = require('spdx-license-list');
+const readline = require('readline');
 
+/* const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+
+rl.question('Type a license name: ', (input) => {
+    const filteredLicenses = Object.keys(licenseList).filter(license => license.toLowerCase().includes(input.toLowerCase()));
+    rl.close(); */
 
 // TODO: Create an array of questions for user input
 const questions = [
@@ -27,19 +38,20 @@ const questions = [
     },
     {
         type: 'list',
-        name: 'License',
+        name: 'license',
         message: 'which License did you use on your project?',
-        choices: ["Academic Free License v3.0", "Apache license 2.0", "Artistic license 2.0", "Boost Software License 1.0", "BSD 2-clause license", "BSD 3-clause license", "Creative Commons Zero v1.0 Universal", "Creative Commons Attribution 4.0", "Creative Commons Attribution Share Alike 4.0", "Do What The Fuck You Want To Public License", "Educational Community License v2.0", "Eclipse Public License 1.0", "Eclipse Public License 2.0", "European Union Public License 1.1", "GNU Affero General Public License v3.0", "GNU General Public License v2.0", "GNU General Public License v3.0",  "GNU Lesser General Public License v2.1", "GNU Lesser General Public License v3.0", "ISC", "LaTeX Project Public License v1.3c", "Microsoft Public License",  "MIT", "Mozilla Public License 2.0", "Open Software License 3.0", "SIL Open Font License 1.1", "University of Illinois/NCSA Open Source License", "The Unlicense", "zLib License"],
+        choices: ["MIT", "apache"],
     },
 ];
 
 inquirer
     .prompt(questions)
     .then(function(userInput) {
-        
-        const markdownString = generateMarkdown(userInput);
+       
+        const markdownString =  generateMarkdown(userInput);
 
         // TODO: Create a function to write README file
+        
         fs.writeFile('README.md', markdownString, function(err) {
             if(err) {
                 console.log(err)
@@ -49,11 +61,10 @@ inquirer
             }
         })
     })
-
-// TODO: Create a function to write README file
-/* function writeToFile(fileName, data) {
+/* }) */
     
-} */
+
+
 
 //TODO: Create a function to initialize app
 function init() {}
