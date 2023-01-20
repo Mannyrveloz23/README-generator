878995f6927aaaa640520eb58693c212e6a39cfe
mannyrveloz23 <you@example.com>
2023-01-19 21:46:17 -0500

    generateMarkdown file done

--- Develop/index.js
+++ Develop/index.js
@@ -15,24 +15,44 @@ const questions = [
     },
     {
         type: 'input',
-        name: 'descripcion',
+        name: 'description',
         message: 'Describe your project',
     },
     {
         type: 'input',
         name: 'installation',
-        message: 'What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.',
+        message: 'How do I install this application?',
     },
     {
         type: 'input',
         name: 'usage',
-        message: 'Provide instructions and examples for use. Include screenshots as needed.',
+        message: 'How do I use this application?',
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'How do I contribute to this application?'
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: 'How do I test this application?'
     },
     {
         type: 'list',
         name: 'license',
         message: 'Choose a license for your project',
-        choices: ["Academic Free License v3.0", "Apache license 2.0", "Artistic license 2.0", "Boost Software License 1.0", "BSD 2-clause license", "BSD 3-clause license", "Creative Commons Zero v1.0 Universal", "Creative Commons Attribution 4.0", "Creative Commons Attribution Share Alike 4.0", "Do What The Fuck You Want To Public License", "Educational Community License v2.0", "Eclipse Public License 1.0", "Eclipse Public License 2.0", "European Union Public License 1.1", "GNU Affero General Public License v3.0", "GNU General Public License v2.0", "GNU General Public License v3.0",  "GNU Lesser General Public License v2.1", "GNU Lesser General Public License v3.0", "ISC", "LaTeX Project Public License v1.3c", "Microsoft Public License",  "MIT", "Mozilla Public License 2.0", "Open Software License 3.0", "SIL Open Font License 1.1", "University of Illinois/NCSA Open Source License", "The Unlicense", "zLib License"],
+        choices: ["Apache 2.0 License", "Boost Software License 1.0", "BSD 3-Clause License", "BSD 2-Clause License", "Eclipse Public License 1.0", "The MIT License", "none"],
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'What is your Github username?',
+    },
+    {
+        type: 'input',
+        name: 'repository',
+        message: 'type the repository link of your project'
     }
 ];
 
@@ -53,7 +73,7 @@ inquirer
             }
         })
     })
-/* }) */
+
     
 
 
